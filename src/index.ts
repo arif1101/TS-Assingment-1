3fd8e6aeedf19282cd3ea0f935b796b1ca949e0b
arif1101 <arifurrahmanarif223@gmail.com>
2025-05-08 21:55:28 +0600

    pb- 4,5,6,7,8

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,103 @@ function formatSrtring(input: string, toUpper?: boolean): string {
     return toUpper ? input.toUpperCase() : input.toLowerCase();
 }
 
-// console.log(formatSrtring("arif"));
 
 function filterByRating(items: { title: string; rating: number }[]): { title: string; rating: number }[] {
     return items.filter(item => item.rating >= 4);
 }
 
-const books = [
-    { title: "Book A", rating: 4.5 },
-    { title: "Book B", rating: 3.2 },
-    { title: "Book C", rating: 5.0 }
-  ];
-
-// console.log(filterByRating(books));
 
 function concatenateArrays<T>(...arrays: T[][]): T[] {
     return arrays.flat();
   }
-//   concatenateArrays(["a", "b"], ["c"]);       // Output: ["a", "b", "c"]
-//   concatenateArrays([1, 2], [3, 4], [5]);     // Output: [1, 2, 3, 4, 5]
+
+
+class Vehicle {
+  private make: string;
+  private year: number;
+
+  constructor(make: string, year: number) {
+    this.make = make;
+    this.year = year;
+  }
+
+  public getInfor(): string {
+    return `Make: ${this.make}, Year: ${this.year}`;
+  }
+}
+
+class Car extends Vehicle {
+  private model: string;
+
+  constructor(make: string, year: number, model: string) {
+    super(make, year);
+    this.model = model;
+  }
   
+  public getModel(): string {
+    return `Model: ${this.model}`;
+  }
+}
+
+
+function processValue(value: string | number): number {
+  if (typeof value === "string"){
+    return value.length;
+  } else {
+    return value * 2;
+  }
+}
+
+
+interface Product {
+  name: string;
+  price: number;
+}
+
+function getMostExpensiveProduct(products: Product[]): Product | null {
+  if (products.length === 0){
+    return null;
+  }
+
+  let mostExpensive = products[0];
+
+  for (const product of products) {
+    if(product.price > mostExpensive.price){
+      mostExpensive = product;
+    }
+  }
+
+  return mostExpensive;
+  
+}
+
+
+enum Day {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
+}
+
+function getDayType(day: Day): string {
+  if(day === Day.Saturday || day === Day.Sunday) {
+    return "Weekend";
+  }
+  return "Weekday";
+}
+ 
+
+async function squareAsync(n: number): Promise<number> {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (n < 0) {
+        reject(new Error("Negative number not allowed"));
+      } else {
+        resolve(n * n);
+      }
+    }, 1000);
+  });
+}
